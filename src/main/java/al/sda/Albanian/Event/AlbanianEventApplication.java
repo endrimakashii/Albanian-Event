3c169c13e503a1de583ebf114e133a49b4d99cd7
endrimakashii <makashiendri@gmail.com>
2024-07-14 13:37:13 +0200

    Initial commit

---
+++ src/main/java/al/sda/Albanian/Event/AlbanianEventApplication.java
@@ -0,0 +1,13 @@
+package al.sda.Albanian.Event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbanianEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbanianEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/sda/Albanian/Event/AlbanianEventApplicationTests.java
@@ -0,0 +1,13 @@
+package al.sda.Albanian.Event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbanianEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
